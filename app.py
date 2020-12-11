7482873216a96295ea842a3f3a36d5ff3c961bd9
Dhya <otmanecherifdhya@gmail.com>
2020-12-11 21:31:44 +0100

    interface avec flask

---
+++ app.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+import os
+import sys
+import searcher
+from flask import Flask, render_template, request, jsonify
+ 
+
+
+
+# create flask instance
+app = Flask(__name__)
+
+
+
+
+# main route
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+# search route
+@app.route('/search', methods=['POST'])
+def search():
+    
+    if request.method == "POST":
+        query = request.form.get('query')
+        result = searcher.searchDoc(query)
+        return render_template("result.html", len = len(result), results = result) 
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', debug=True)
--- searcher.py
+++ searcher.py
@@ -4,35 +4,38 @@ import docproc
 from bs4 import BeautifulSoup
 import os
 
-print("loading files")
-doc_list=[]
-for filename in os.listdir(os.getcwd()+"/corpus"):
-  with open(os.getcwd()+"/corpus/"+filename, 'r') as F:
-    soup = BeautifulSoup(F,'html.parser')
-    for doc in soup('doc'):
-        doc_list.append(doc)
+def searchDoc(query):
+ 
+  print("loading files")
+  doc_list=[]
+  for filename in os.listdir(os.getcwd()+"/corpus"):
+    with open(os.getcwd()+"/corpus/"+filename, 'r') as F:
+      soup = BeautifulSoup(F,'html.parser')
+      for doc in soup('doc'):
+          doc_list.append(doc)
 
-print("load dict")
-dictionary = gensim.corpora.Dictionary()
-dictionary.load('corpus.dict')
+  print("load dict")
+  dictionary = gensim.corpora.Dictionary().load('corpus.dict')
+  #dictionary.load('corpus.dict')
 
-mmcorpus = gensim.corpora.MmCorpus('corpus.mm')
+  mmcorpus = gensim.corpora.MmCorpus('corpus.mm')
 
-print("load model/index")
-model = gensim.models.TfidfModel.load("corpus.model")
-index = gensim.similarities.MatrixSimilarity.load("corpus.index")
+  print("load model/index")
+  model = gensim.models.TfidfModel.load("corpus.model")
+  index = gensim.similarities.MatrixSimilarity.load("corpus.index")
 
-#doc_list=np.load('doclist.npy')
+  #doc_list=np.load('doclist.npy')
 
-Q = input("Query: ")
-pQ = docproc.ProcessDocument(Q)
-vQ = dictionary.doc2bow(pQ)
+  #Q = input("Query: ")
+  Q=query
+  pQ = docproc.ProcessDocument(Q)
+  vQ = dictionary.doc2bow(pQ)
 
-wQ = model[vQ]
-similarities = index[wQ]
-ranking = sorted(enumerate(similarities), key=itemgetter(1), reverse=True)
-
-
-
-for c, s in ranking[:10]:
-    print("[ Score = %.3f" %s +" ] " + doc_list[c].find('docno').text)
+  wQ = model[vQ]
+  similarities = index[wQ]
+  ranking = sorted(enumerate(similarities), key=itemgetter(1), reverse=True)
+  result = []
+  for c, s in ranking[:10]:
+      result.append(doc_list[c])
+      print("[ Score = %.3f" %s +" ] " + doc_list[c].find('docno').text)
+  return result
